8bcb8547e2ddfd1282ade0cad6e1e6f730a6550b
illia <illialotfalian@gmail.com>
2023-08-08 16:27:56 -0400

    Initial Commit

---
+++ imageGeneratorApp/imageGeneratorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageGeneratorAppApp.swift
+//  imageGeneratorApp
+//
+//  Created by Illia Lotfalian on 2023-08-08.
+//
+
+import SwiftUI
+
+@main
+struct imageGeneratorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
